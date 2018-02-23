2b4a36312239314c0545de1a19e4192056ff7a2f
Kosara Golemshinska <gusgolko@student.gu.se>
2018-02-23 20:51:00 +0100

    Start messages (console)

--- GoPass/ConsoleApp/src/Main.java
+++ GoPass/ConsoleApp/src/Main.java
@@ -14,6 +14,13 @@ public class Main {
         // Print welcome message and prompt for username and full name.
         System.out.println("Welcome to GoPass!");
         System.out.println();
+        System.out.println("GoPass generates strong passwords so you don't have to!");
+        System.out.println("+ 8 characters in length");
+        System.out.println("+ Mixed case");
+        System.out.println("+ Alphanumeric");
+        System.out.println("+ Special symbols");
+        System.out.println("+ Different from your username and name");
+        System.out.println();
         System.out.print("Input your username to get started: ");
         username = scanner.nextLine();
         System.out.println();
