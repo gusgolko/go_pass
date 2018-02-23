69e26cc32a70f7976c1bc899a3ee1b1b728b2c4d
Kosara Golemshinska <gusgolko@student.gu.se>
2018-02-23 21:51:54 +0100

    Basic logic in place. TODO: check if same as the names and avoid consecutive characters of the same type (console)

--- GoPass/ConsoleApp/src/Generator.java
+++ GoPass/ConsoleApp/src/Generator.java
@@ -1,9 +1,23 @@
 package src;
 
+import java.security.SecureRandom;
+
 /**
  * This class contains the logic for the strong password generation.
  */
 public class Generator {
+
+    // SecureRandom number generator which makes the password harder to guess.
+    private static SecureRandom random_int = new SecureRandom();
+
+    // We group the characters to be used into constant strings.
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBERS = "0123456789";
+    private static final String SYMBOLS = "!?#$%/^&@+-=";
+
+    // We'll use this to select a random list.
+    private static String[] characters = {UPPERCASE, LOWERCASE, NUMBERS, SYMBOLS};
 
     /**
      * This class keeps track of the strings to avoid and generates the password.
@@ -14,7 +28,19 @@ public class Generator {
     public static String generatePassword(String username, String full_name) {
 
         // This is the string where we'll store the generated password.
-        String pass = ".";  // Placeholder dot for now.
+        String pass = "";
+
+        /*
+         * We loop 8 times for each random character.
+         * First, we select a random list to pick from using SecureRandom.
+         * Then, we generate a random index and concatenate the character
+         * at the index to the final password.
+         */
+        for(int i = 0; i < 8; i++) {
+            int current = random_int.nextInt(characters.length);
+            int index = random_int.nextInt(characters[current].length());
+            pass += characters[current].charAt(index);
+        }
 
         return pass;
 
