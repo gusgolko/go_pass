818544c1d8afeac2be5049c0711551061663cb64
Kosara Golemshinska <gusgolko@student.gu.se>
2018-03-02 18:48:57 +0100

    Changing the program logic

--- GoPass/ConsoleApp/src/Generator.java
+++ GoPass/ConsoleApp/src/Generator.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
 
 /**
  * This class contains the logic for the strong password generation.
@@ -10,14 +11,20 @@ public class Generator {
     // SecureRandom number generator which makes the password harder to guess.
     private static SecureRandom random_int = new SecureRandom();
 
-    // We group the characters to be used into constant strings.
-    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
-    private static final String NUMBERS = "0123456789";
-    private static final String SYMBOLS = "!?#$%/^&@+-=";
+    // We group the characters to be used into constant character arrays.
+    private static final char[] UPPERCASE = {'A', 'B', 'C', 'D', 'E', 'F', 'G',
+            'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z'};
+    private static final char[] LOWERCASE = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+            'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z'};
+    private static final char[] NUMBERS = {'0', '1', '2', '3', '4', '5', '6',
+            '7', '8', '9'};
+    private static final char[] SYMBOLS = {'!', '?', '#', '$', '%', '/', '^',
+            '&', '@', '+', '-', '='};
 
     // We'll use this to select a random list.
-    private static String[] characters = {UPPERCASE, LOWERCASE, NUMBERS, SYMBOLS};
+    private static char[][] characters = {UPPERCASE, LOWERCASE, NUMBERS, SYMBOLS};
 
     /**
      * This class keeps track of the strings to avoid and generates the password.
@@ -28,8 +35,10 @@ public class Generator {
      */
     public static String generatePassword(String username, String f_name, String l_name) {
 
-        // This is the string where we'll store the generated password.
-        String pass = "";
+        // This is the character array where we'll store the generated password.
+        char[] pass = new char[8];
+        // This is the String we'll return.
+        String password;
 
         /*
          * We loop 8 times for each random character.
@@ -38,20 +47,21 @@ public class Generator {
          * at the index to the final password.
          */
         for (int i = 0; i < 8; i++) {
-            int current = random_int.nextInt(characters.length);
-            int index = random_int.nextInt(characters[current].length());
-            pass += characters[current].charAt(index);
+            int current = random_int.nextInt(characters.length);//rand int 0-3
+            int index = random_int.nextInt(characters[current].length);//rand int 0-whatever list
+            pass[i] =  characters[current][index];
         }
 
         /* Check if any of the names are contained in the random password
          * to avoid making it vulnerable accidentally.
          */
-        if (pass.contains(username) || pass.contains(f_name) || pass.contains(l_name)) {
+        if (Arrays.asList(pass).contains(username) || Arrays.asList(pass).contains(f_name) ||
+                Arrays.asList(pass).contains(l_name)) {
             // Recursive call to generate a new random password.
             pass = generatePassword(username, f_name, l_name);
         }
 
-        return pass;
+        return password;
 
     }
 }
