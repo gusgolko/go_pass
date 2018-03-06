70251dd0edceefecdd6c3c47e10d06545c754058
Kosara Golemshinska <gusgolko@student.gu.se>
2018-03-06 17:09:17 +0100

    compile and run script added

--- GoPass/ConsoleApp/run.sh
+++ GoPass/ConsoleApp/run.sh
@@ -2,12 +2,11 @@
 
 #################################
 ## Author: Kosara Golemshinska ##
-##### Modified: 2018-03-04 ######
+##### Modified: 2018-03-06 ######
 #################################
 
-javac ~/go_pass/GoPass/ConsoleApp/src/Generator.java
-javac ~/go_pass/GoPass/ConsoleApp/src/Main.java
+javac -d ~/go_pass/GoPass/classes/ ~/go_pass/GoPass/ConsoleApp/src/*.java
 
-cd ~/go_pass/GoPass/ConsoleApp
+cd ~/go_pass/GoPass/classes/
 
 java -cp . src/Main
