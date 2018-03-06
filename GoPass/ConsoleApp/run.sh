99ed805a8a5b7fedcaa9b7faf55d25ca353b90ae
Kosara Golemshinska <gusgolko@student.gu.se>
2018-03-06 17:19:23 +0100

    now making classes directory before compiling

--- GoPass/ConsoleApp/run.sh
+++ GoPass/ConsoleApp/run.sh
@@ -5,6 +5,8 @@
 ##### Modified: 2018-03-06 ######
 #################################
 
+cd ~/go_pass/GoPass/ && mkdir classes
+
 javac -d ~/go_pass/GoPass/classes/ ~/go_pass/GoPass/ConsoleApp/src/*.java
 
 cd ~/go_pass/GoPass/classes/
