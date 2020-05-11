d4756359d043883332e4f888dbcea0a1162ac22f
Kosara Golemshinska <golemshinska.kosara@gmail.com>
2020-05-11 21:15:56 +0200

    Modified start script and readme

--- GoPass/ConsoleApp/run.sh
+++ GoPass/ConsoleApp/run.sh
@@ -2,11 +2,13 @@
 
 #################################
 ## Author: Kosara Golemshinska ##
-##### Modified: 2018-06-05 ######
+##### Modified: 2020-05-11 ######
 #################################
 
-cd ~/go_pass/GoPass/
 
+cd ..
+
+# Make a directory for the bytecode if one doesn't exist.
 if [ ! -d classes ]
 then
 	mkdir classes
@@ -15,8 +17,13 @@ else
 	mkdir classes
 fi
 
-javac -d ~/go_pass/GoPass/classes/ ~/go_pass/GoPass/ConsoleApp/src/*.java
+# Set the path to the bytecode directory.
+START_PATH=$(pwd)
+CLASS_LOC=${START_PATH}/classes/
+cd $CLASS_LOC
 
-cd ~/go_pass/GoPass/classes/
+# Compile the files in the bytecode directory.
+javac -d $CLASS_LOC ${START_PATH}/ConsoleApp/src/*.java
 
+# Run the app.
 java -cp . src/Main
--- README.md
+++ README.md
@@ -6,18 +6,26 @@ GoPass is a Java commandline app that generates strong passwords.
 
 ### Get started
 
-Clone the repo in your home folder:
+Clone the repo:
 
 ```git
-$ git clone https://github.com/gusgolko/go_pass.git
+$ git clone https://github.com/kosara-g/go_pass.git
 ```
 
 To run the app with _bash_ if you're using Linux:
 
-Navigate to /go_pass/GoPass/ConsoleApp and run run.sh:
+Navigate to /go_pass/GoPass/ConsoleApp and run `run.sh`:
 
 ```shell
 $ sh run.sh
 ```
+
+Alternatively:
+
+```shell
+$ ./run.sh
+```
+**Note:** It's important that you run the script as described.
+It uses the path from where you run it to navigate and set the structure.
 
 You're all set!
